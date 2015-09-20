6e59eb8d835a9e2b1f3f9ebdd3dd765bd41ac778
Purnima Gupta <pooyax14@gmail.com>
2015-09-21 04:54:03 +0530

    Add PHP includes

---
+++ includes/styles.php
@@ -0,0 +1,10 @@
+<style>
+    body {
+        font-family: ubuntu, sans-serif;
+        margin: 2rem 10rem;
+    }
+    label {
+        display: inline-block;
+        min-width: 6rem;
+    }
+</style>
